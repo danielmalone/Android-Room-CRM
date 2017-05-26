26b9e3c73d646ebd447adb204e4676d8804d530b
Daniel Malone <danieljmalone@gmail.com>
2017-05-25 23:32:48 -0500

    add Add Person screen

---
+++ app/src/main/java/com/finepointmobile/simplecrm/AddPerson.java
@@ -0,0 +1,18 @@
+package com.finepointmobile.simplecrm;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by danielmalone on 5/25/17.
+ */
+
+public class AddPerson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_person);
+    }
+}
